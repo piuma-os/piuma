c371b256025283f0a9d00cd16119df4bcef3a85c
Matilde Morrone <contact@morrone.dev>
2025-02-10 20:53:26 +0100

    Output serial to stdout

--- qemu.sh
+++ qemu.sh
@@ -5,4 +5,4 @@ qemu-system-x86_64 -m 1G -enable-kvm -machine q35 -device intel-iommu -cpu host 
     -drive file=disk.qcow2,if=virtio \
     -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2-ovmf/x64/OVMF_CODE.4m.fd \
     -drive if=pflash,format=raw,file=OVMF_VARS.4m.fd \
-    -device virtio-vga-gl -display gtk,gl=on
+    -device virtio-vga-gl -display gtk,gl=on -serial stdio
